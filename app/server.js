4fcfed067ec2be7f26a4bf94c999ba5556ba6477
diffaimajid <diffa.imajid16@gmail.com>
2020-05-16 14:29:00 +0700

    Add ENV to yaml & additional info to display

--- app/server.js
+++ app/server.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var app = express();
 var os = require("os");
-var morgan  = require('morgan');
+var morgan = require('morgan');
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use(express.static('static'));
 app.use(morgan('combined'));
@@ -12,14 +12,26 @@ app.use(morgan('combined'));
 // Configuration
 var port = process.env.PORT || 8080;
 var message = process.env.MESSAGE || "Hello world!";
+var nodeName = process.env.MY_NODE_NAME || "nodeName";
+var nodeIP = process.env.MY_NODE_IP || "nodeIP";
+var podName = process.env.MY_POD_NAME || "podName";
+var podNamespace = process.env.MY_POD_NAMESPACE || "podNamespace";
+var podIP = process.env.MY_POD_IP || "podIP";
+var podServiceAccount = process.env.MY_POD_SERVICE_ACCOUNT || "podServiceAccount";
 
 app.get('/', function (req, res) {
-    res.render('home', {
-      message: message,
-      platform: os.type(),
-      release: os.release(),
-      hostName: os.hostname()
-    });
+  res.render('home', {
+    message: message,
+    platform: os.type(),
+    release: os.release(),
+    hostName: os.hostname(),
+    nodeName: nodeName,
+    nodeIP: nodeIP,
+    podName: podName,
+    podNamespace: podNamespace,
+    podIP: podIP,
+    podServiceAccount: podServiceAccount,
+  });
 });
 
 // Set up listener
